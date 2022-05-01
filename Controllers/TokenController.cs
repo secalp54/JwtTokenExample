83e9adb29077f7ed2c29baeff7132336290e7e9b
secalp <secalp54@gmail.com>
2022-05-01 12:00:55 +0300

    firs commit

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TokenExemble.DAL;
+
+namespace TokenExemble.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("",new BuiltToken().createToken());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult PageOne()
+        {
+            return Ok("Sayfa 1 için giriş tamam");
+        }
+    }
+}
---
+++ DAL/BuiltToken.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace TokenExemble.DAL
+{
+    public class BuiltToken
+    {
+        public string createToken()
+        {
+            //startup tarafından tanımlanan anahtar değerimiz çağrılır
+            var bytes = Encoding.UTF8.GetBytes("alperalbostansa!5054774994");
+            SymmetricSecurityKey key= new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(issuer:"http://localhost",audience:"http://localhost",notBefore:DateTime.Now,expires:DateTime.Now.AddMinutes(20),signingCredentials:credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}
